22a8866fb2bae624a05eed21b336373770ec42ec
Raj Kumar <76458820+stickqz@users.noreply.github.com>
2025-08-09 17:49:08 +0000

    lc 358

---
+++ Leetcode_problems/358.rearrange_string_k_distance_apart.py
@@ -0,0 +1,49 @@
+from collections import Counter, deque
+from heapq import heappop, heappush
+''' 358. Rearrange String k Distance Apart
+Given a non-empty string s and an integer k, rearrange the string such that the same characters are at least distance k from each other.
+All input strings are given in lowercase letters. If it is not possible to rearrange the string, return an empty string "".
+
+Example 1:  Input: s = "aabbcc", k = 3
+Output: "abcabc"
+Explanation: The same letters are at least distance 3 from each other.
+
+Example 2:  Input: s = "aaabc", k = 3
+Output: ""
+Explanation: It is not possible to rearrange the string.
+
+Example 3:  Input: s = "aaadbbcc", k = 2
+Output: "abacabcd"
+Explanation: The same letters are at least distance 2 from each other.
+'''
+class solution:
+    def rearrange_string(s, k):
+        count = Counter(s)
+        maxh = []
+
+        for ch, c in count.items():
+            heappush(maxh, (-c, ch))
+
+        q = deque()
+        ans = []
+        t = 0
+
+
+        while maxh or q:
+            t += 1
+
+            if maxh:
+                c, ch = heappop(maxh)
+                ans.append(ch)
+                c += 1
+
+                if c:
+                    q.append((c, ch, t + k))
+            else:
+                return ""
+            
+            if q and q[0][2] <= t:
+                c, ch, tm = q.popleft()
+                heappush(maxh, (c, ch))
+        
+        return ''.join(ans)
